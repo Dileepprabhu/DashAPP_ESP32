d7dafc1d4622abf7d143ba63b4cf2aec208c34cb
Dileep <prd3nh@bosch.com>
2024-12-27 08:05:45 +0530

    Made changes to have real time update, this is cod e is from GPT40, but still not working

--- Dash_Dinga_app.py
+++ Dash_Dinga_app.py
@@ -5,6 +5,7 @@ import dash_bootstrap_components as dbc
 import asyncio
 import websockets
 import threading
+from queue import Queue
 
 # Initialize the Dash app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -21,24 +22,31 @@ app.layout = dbc.Container([
         dbc.Col(dbc.Button("Start Counter", id='start-counter', color="primary"), className="mb-2"),
         dbc.Col(dbc.Button("Stop Counter", id='stop-counter', color="danger"), className="mb-2"),
         dbc.Col(dbc.Button("Start Motor Test", id='start-motor-test', color="success"), className="mb-2")
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Interval(id='interval-component', interval=500, n_intervals=0), className="mb-2")  # Update every 500ms
     ])
 ])
 
 # WebSocket URL
 ws_url = "ws://192.168.4.1/ws"
 
-# Global variable to store live data
-live_data = []
+# Thread-safe queue to store live data
+live_data_queue = Queue()
 
 # Function to handle WebSocket communication
 async def websocket_handler():
-    global live_data
     async with websockets.connect(ws_url) as websocket:
         while True:
             message = await websocket.recv()
-            live_data.append(message)
-            if len(live_data) > 100:  # Limit the number of messages to 100
-                live_data.pop(0)
+            live_data_queue.put(message)
+            if live_data_queue.qsize() > 100:  # Limit the number of messages to 100
+                live_data_queue.get()
+
+# Function to send message to WebSocket
+async def send_message(message):
+    async with websockets.connect(ws_url) as websocket:
+        await websocket.send(message)
 
 # Start WebSocket handler in a separate thread
 def start_websocket_thread():
@@ -49,30 +57,30 @@ def start_websocket_thread():
 threading.Thread(target=start_websocket_thread, daemon=True).start()
 
 # Callback to update live data
-@app.callback(Output('live-data', 'children'), [Input('start-counter', 'n_clicks'),
-                                                Input('stop-counter', 'n_clicks'),
-                                                Input('start-motor-test', 'n_clicks')])
-def update_live_data(start_counter, stop_counter, start_motor_test):
-    ctx = dash.callback_context
-
-    if not ctx.triggered:
-        button_id = 'No clicks yet'
-    else:
-        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-
-    if button_id == 'start-counter':
-        asyncio.run(send_message("Task_counter_start"))
-    elif button_id == 'stop-counter':
-        asyncio.run(send_message("Task_counter_stop"))
-    elif button_id == 'start-motor-test':
-        asyncio.run(send_message("Motor_Test_Start"))
+@app.callback(Output('live-data', 'children'), [Input('interval-component', 'n_intervals')])
+def update_live_data(n_intervals):
+    live_data = []
+    while not live_data_queue.empty():
+        live_data.append(live_data_queue.get())
 
     return [html.Div(msg) for msg in live_data]
 
-# Function to send message to WebSocket
-async def send_message(message):
-    async with websockets.connect(ws_url) as websocket:
-        await websocket.send(message)
+# Callback to handle button interactions
+@app.callback([Output('interval-component', 'disabled')], 
+              [Input('start-counter', 'n_clicks'), Input('stop-counter', 'n_clicks'), Input('start-motor-test', 'n_clicks')])
+def handle_buttons(start_counter, stop_counter, start_motor_test):
+    ctx = dash.callback_context
+    if ctx.triggered:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+        if button_id == 'start-counter':
+            threading.Thread(target=asyncio.run, args=(send_message("Task_counter_start"),)).start()
+        elif button_id == 'stop-counter':
+            threading.Thread(target=asyncio.run, args=(send_message("Task_counter_stop"),)).start()
+        elif button_id == 'start-motor-test':
+            threading.Thread(target=asyncio.run, args=(send_message("Motor_Test_Start"),)).start()
+
+    return [False]  # Ensure interval is always enabled for live updates
 
 # Run the app
 if __name__ == '__main__':
